526a60bed4064f99a533305907607027cb41102b
s6210110342 <6210110342@psu.ac.th>
2021-07-12 13:17:54 +0700

    section 6

---
+++ screen/ZipCodeScreen.js
@@ -0,0 +1,48 @@
+import { useNavigation } from '@react-navigation/native';
+import React from 'react';
+import { FlatList, StyleSheet, Text, TouchableHighlight, View } from 'react-native';
+
+
+const availableZipItems = [
+    { place: 'Hatyai', code: '90110' },
+    { place: 'Trang', code: '92000' },
+    { place: 'Chiangmai', code: '50000' },
+    { place: 'Khonkaen', code: '40000' },
+    { place: 'Chonburi', code: '20000' },
+   ]
+
+const ZipItem = ({place, code, navigation}) => (
+    <TouchableHighlight onPress={() => {
+        navigation.navigate('Weather', {zipCode: code})
+    }}>
+        <View style={styles.ZipItem}>
+            <Text>{place}</Text>
+            <Text>{code}</Text>
+        </View>
+    </TouchableHighlight>
+)
+export default function ZipCodeScreen(){
+    const navigation = useNavigation()
+    return(
+        <FlatList
+            data = {availableZipItems}
+            keyExtractor = {item => item.code}
+            renderItem = {({item}) => <ZipItem {...item} navigation={navigation}/>}
+        />
+    )
+}
+
+const styles = StyleSheet.create({
+    ZipItem: {
+        flex: 1,
+        flexDirection: 'row',
+        justifyContent: 'space-between'
+    },
+    zipPlace: {
+        flex: 1,
+    },
+    zipCode: {
+        flex: 1,
+    }
+    
+})
